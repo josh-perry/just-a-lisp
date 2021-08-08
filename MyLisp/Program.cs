0a57fe0d4c7a244e87d3e6baf867e238223091c2
Josh Perry <joshperry@outlook.com>
2021-08-08 23:29:51 +0100

    Initial commit

---
+++ MyLisp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyLisp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
